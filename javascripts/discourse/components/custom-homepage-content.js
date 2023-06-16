84f8b67392b1d9030e3520358eb03998e61baa57
dekunledev <adekunle.odujoko@flutterwavego.com>
2023-06-16 13:19:37 +0100

    Testing Custom Homepage Contents

---
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -0,0 +1,15 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+import { defaultHomepage } from "discourse/lib/utilities";
+
+export default class CustomHomepageContent extends Component {
+  constructor() {
+    super(...arguments);
+    this.router = service();
+  }
+
+  get isHomepage() {
+    const currentRouteName = this.router.currentRouteName;
+    return currentRouteName === `discovery.${defaultHomepage()}`;
+  }
+}

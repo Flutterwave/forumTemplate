993e74bcede0e544b22c6951aa71339b66680325
dekunledev <adekunle.odujoko@flutterwavego.com>
2023-06-19 08:15:26 +0100

    Testing Custom Homepage Contents

--- javascripts/discourse/components/custom-homepage-content.js
+++ javascripts/discourse/components/custom-homepage-content.js
@@ -3,14 +3,15 @@ import { inject as service } from "@ember/service";
 import { defaultHomepage } from "discourse/lib/utilities";
 
 export default class CustomHomepageContent extends Component {
-  constructor() {
-    super(...arguments);
-    this.router = service();
-  }
+  // constructor() {
+  //   super(...arguments);
+  //   this.router = service();
+  // }
+  @service router;
 
   get isHomepage() {
     const currentRouteName = this.router.currentRouteName;
-    console.log(`${this.router}, discovery.${defaultHomepage()}`);
+    // console.log(`${this.router}, discovery.${defaultHomepage()}`);
     return currentRouteName === `discovery.${defaultHomepage()}`;
   }
 }

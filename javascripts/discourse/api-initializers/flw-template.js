82592032d1f30e9d48056a779c5806e02c2b4446
ChijiokeFLW <--global>
2023-06-26 21:12:15 +0100

    Updated code that restructures post details header template

--- javascripts/discourse/api-initializers/flw-template.js
+++ javascripts/discourse/api-initializers/flw-template.js
@@ -3,12 +3,12 @@ import { findRawTemplate } from "discourse-common/lib/raw-templates";
 import { htmlSafe } from "@ember/template";
 
 export default {
-  name: "fkbpro",
+  name: "FlwTemplate",
   initialize() {
     withPluginApi("0.8.7", (api) => {
       // Use same template on Desktop and MobileView
       api.modifyClass("component:topic-list-item", {
-        pluginId: "fkb-template",
+        pluginId: "flw-template",
 
         renderTopicListItem() {
           const template = findRawTemplate("list/custom-topic-list-item");

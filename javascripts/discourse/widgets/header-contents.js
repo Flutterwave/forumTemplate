d4d041812dbcce7c642b73fb7f3e2aaf9ce96a13
ChijiokeFLW <chijioke.okorji@flutterwavego.com>
2023-08-22 18:58:15 +0100

    Added appropriate closing to handlebars if-statement

---
+++ javascripts/discourse/widgets/header-contents.js
@@ -0,0 +1,31 @@
+import { createWidget } from "discourse/widgets/widget";
+import hbs from "discourse/widgets/hbs-compiler";
+
+createWidget("header-contents", {
+  tagName: "div.contents.clearfix",
+  transform() {
+    return {
+      showBootstrapMode: this.currentUser?.staff && this.site.desktopView,
+    };
+  },
+  template: hbs`
+    {{#if this.site.desktopView}}
+      {{#if attrs.sidebarEnabled}}
+        {{sidebar-toggle attrs=attrs}}
+      {{/if}}
+    {{/if}}
+
+    {{home-logo attrs=attrs}}
+
+    {{#if attrs.topic}}
+      {{floating-search-input attrs=attrs}}
+      {{#if this.siteSettings.bootstrap_mode_enabled}}
+        {{#if transformed.showBootstrapMode}}
+          {{header-bootstrap-mode attrs=attrs}}
+        {{/if}}
+      {{/if}}
+    {{/if}}
+
+    <div class="panel clearfix" role="navigation">{{yield}}</div>
+  `,
+});
